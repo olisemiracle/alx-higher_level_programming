54cb3eec51ab6988fcbb1d1b590dd834e1918920
olisemiracle <olisemiracle904@gmail.com>
2023-03-06 23:15:36 -0800

    checks if a singly linked list is a cycle or not

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked is a cycle or not
+ * @list: The head node
+ * Return: 1 if it's a cycle or 0 if it's not
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *temp;
+
+	temp = list;
+	temp = temp->next;
+	while (temp != NULL && temp != list)
+	{
+		if (temp->next == NULL)
+			return (0);
+		if (temp->next == list)
+			return (1);
+		temp = temp->next;
+	}
+	if (temp->next == NULL)
+		return (0);
+	return (1);
+}
